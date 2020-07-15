594d5a0099afa36a88662684eb38b807ca407ec0
MaxTratnik <max.tratnik@gmail.com>
2020-07-15 09:31:13 +0200

    Sets up an Adventure-Player (unfinished)

--- TextAdventureProject/TextAdventureProject/Program.cs
+++ TextAdventureProject/TextAdventureProject/Program.cs
@@ -8,5 +8,31 @@ namespace TextAdventureProject
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        public static void WriteColored(string text, ConsoleColor textColor)
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+
+            Console.Write(text);
+
+            Console.ForegroundColor = c;
+        }
+
+        public static void WriteLineColored(string text, ConsoleColor textColor)
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = c;
+        }
+
+        public static int ReadNumericInput(int maxNumber, int minNumber = 1)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ TextAdventureProject/TextAdventureProject/TextAdventureFramework/AdventurePlayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace TextAdventureProject.TextAdventureFramework
+{
+    class AdventurePlayer
+    {
+        private const int DefaultStatValue = 8;
+        public string Name;
+
+        public int Str { get; private set; }
+        public int Dex { get; private set; }
+        public int Int { get; private set; }
+
+        public AdventurePlayer(string name)
+        {
+            Name = name;
+
+            Str = DefaultStatValue;
+            Dex = DefaultStatValue;
+            Int = DefaultStatValue;
+        }
+
+
+        public static AdventurePlayer Generate(int statPoints = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SetStat(PlayerStat stat, int value)
+        {
+            switch (stat)
+            {
+                case PlayerStat.STR:
+                    Str = value;
+                    break;
+                case PlayerStat.DEX:
+                    Dex = value;
+                    break;
+                case PlayerStat.INT:
+                    Int = value;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stat), stat, null);
+            }
+        }
+
+        private int GetPlayerStatValueByStat(PlayerStat stat)
+        {
+            switch (stat)
+            {
+                case PlayerStat.STR:
+                    return Str;
+                case PlayerStat.DEX:
+                    return Dex;
+                case PlayerStat.INT:
+                    return Int;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stat), stat, null);
+            }
+        }
+    }
+
+    public enum PlayerStat
+    {
+        STR,
+        DEX,
+        INT
+    }
+}
